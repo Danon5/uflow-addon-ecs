522c560bc23f802bee40dd8465eca154b3fe0fcc
Danon5 <danonfive@gmail.com>
2023-06-24 00:00:30 -0700

    - Progress on NetSync abstraction

---
+++ Core/Code/Runtime/Unity/Modules/BaseEcsModule.cs
@@ -0,0 +1,9 @@
+﻿using UFlow.Core.Runtime;
+
+namespace UFlow.Addon.Ecs.Core.Runtime.Modules {
+    public abstract class BaseEcsModule : BaseModule {
+        public World LocalWorld { get; private set; }
+        
+        
+    }
+}
