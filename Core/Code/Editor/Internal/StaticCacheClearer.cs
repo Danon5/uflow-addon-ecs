f52cc95b299ec38ecf7c2c8680f73bb7a4d290a0
Danon5 <danonfive@gmail.com>
2023-06-25 04:00:21 -0700

    - Added delegates for events + organized

--- Core/Code/Editor/Internal/StaticCacheClearer.cs
+++ Core/Code/Editor/Internal/StaticCacheClearer.cs
@@ -1,10 +1,11 @@
-﻿using UnityEngine;
+﻿using UFlow.Addon.Ecs.Core.Runtime;
+using UnityEngine;
 
 namespace UFlow.Addon.Ecs.Core.Editor {
     internal static class StaticCacheClearer {
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
         private static void InitializeOnLoad() {
-            
+            Publishers<StaticCacheClearEvent>.Global.Publish(new StaticCacheClearEvent());
         }
     }
 }
