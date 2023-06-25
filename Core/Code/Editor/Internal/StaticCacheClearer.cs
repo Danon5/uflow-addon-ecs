a3013a7956db6dcbc35775e469778001e8ce78c2
Danon5 <danonfive@gmail.com>
2023-06-25 03:49:42 -0700

    - Fixed editor assemblies missing "Core" name

---
+++ Core/Code/Editor/Internal/StaticCacheClearer.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace UFlow.Addon.Ecs.Core.Editor {
+    internal static class StaticCacheClearer {
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
+        private static void InitializeOnLoad() {
+            
+        }
+    }
+}
