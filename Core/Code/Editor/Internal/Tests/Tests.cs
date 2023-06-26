329999c29366e7fd2b315cfb8a55c595ed4cfc8c
Danon5 <danonfive@gmail.com>
2023-06-26 04:27:42 -0700

    - Removed UFlow dependencies

---
+++ Core/Code/Editor/Internal/Tests/Tests.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using UFlow.Addon.Ecs.Core.Runtime;
+
+namespace UFlow.Addon.Ecs.Core.Editor {
+    internal static class Tests {
+        private static World m_world;
+        
+        [SetUp]
+        public static void SetUp() {
+            m_world = World.Create();
+        }
+
+        [TearDown]
+        public static void TearDown() {
+            m_world.Destroy();
+            m_world = null;
+        }
+        
+        [Test]
+        public static void BasicEntityTest() {
+            var entities = new Queue<Entity>();
+
+            for (var i = 0; i < 100; i++)
+                entities.Enqueue(m_world.CreateEntity());
+            
+            while (entities.TryDequeue(out var entity))
+                entity.Destroy();
+        }
+    }
+}
