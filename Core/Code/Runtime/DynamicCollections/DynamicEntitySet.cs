74375020c1e0d6b30e3ab33982df425563c7a874
Danon5 <danonfive@gmail.com>
2023-06-26 03:28:33 -0700

    - Added basic DynamicEntitySet (WIP)

--- Core/Code/Runtime/DynamicCollections/DynamicEntitySet.cs
+++ Core/Code/Runtime/DynamicCollections/DynamicEntitySet.cs
@@ -1,5 +1,59 @@
-﻿namespace UFlow.Addon.Ecs.Core.Runtime {
-    public sealed class DynamicEntitySet {
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UFlow.Core.Runtime;
+
+namespace UFlow.Addon.Ecs.Core.Runtime {
+    public sealed class DynamicEntitySet : IEnumerable<Entity>, IDisposable {
+        private readonly World m_world;
+        private readonly Predicate<ComponentBitset> m_filter;
+        private readonly IDisposable m_subscriptions;
+        private readonly SparseSet<Entity> m_entities;
+
+        internal DynamicEntitySet(in World world, Predicate<ComponentBitset> filter) {
+            m_world = world;
+            m_filter = filter;
+            m_subscriptions = new List<IDisposable> {
+                world.SubscribeEntityComponentAdded(CheckedAdd),
+                world.SubscribeEntityComponentRemoved(CheckedRemove),
+                world.SubscribeEntityDestroyed(Remove),
+                world.SubscribeWorldDestroyed(Dispose)
+            }.MergeIntoGroup();
+            m_entities = new SparseSet<Entity>();
+
+            foreach (var entity in m_world.GetEntitiesEnumerable())
+                CheckedAdd(entity);
+        }
+
+        public IEnumerator<Entity> GetEnumerator() {
+            foreach (var entity in m_entities)
+                yield return entity;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() {
+            return GetEnumerator();
+        }
+
+        public void Dispose() {
+            m_entities.Dispose();
+            m_subscriptions.Dispose();
+        }
+
+        private void CheckedAdd(in Entity entity) {
+            if (m_entities.Has(entity.id)) return;
+            if (!m_filter(entity.ComponentBitset)) return;
+            m_entities.Set(entity.id, entity);
+        }
         
+        private void CheckedRemove(in Entity entity) {
+            if (!m_entities.Has(entity.id)) return;
+            if (m_filter(entity.ComponentBitset)) return;
+            m_entities.Remove(entity.id);
+        }
+        
+        private void Remove(in Entity entity) {
+            if (!m_entities.Has(entity.id)) return;
+            m_entities.Remove(entity.id);
+        }
     }
 }
