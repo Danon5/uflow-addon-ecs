6c7faf56a0eec7675500eb49c0cd4025f98d3600
Danon5 <danonfive@gmail.com>
2023-07-01 06:35:40 -0700

    - Set up useful system groups

---
+++ Core/Code/Runtime/Unity/Modules/EcsModule.cs
@@ -0,0 +1,31 @@
+﻿using UFlow.Core.Runtime;
+
+namespace UFlow.Addon.Ecs.Core.Runtime {
+    public sealed class EcsModule : BaseBehaviourModule {
+        public World World { get; private set; }
+        
+        public override void Load() {
+            World = EcsUtils.Worlds.CreateWorldFromType<DefaultWorld>();
+            World.SetupSystemGroups();
+        }
+
+        public override void Unload() {
+            World.Destroy();
+        }
+
+        public override void Update() {
+            World.RunSystemGroup<FrameSimulationSystemGroup>();
+            World.RunSystemGroup<FrameRenderSystemGroup>();
+        }
+
+        public override void FixedUpdate() {
+            World.RunSystemGroup<FixedSimulationSystemGroup>();
+            World.RunSystemGroup<FixedRenderSystemGroup>();
+        }
+
+        public override void LateUpdate() {
+            World.RunSystemGroup<LateFrameSimulationSystemGroup>();
+            World.RunSystemGroup<LateFrameRenderSystemGroup>();
+        }
+    }
+}
