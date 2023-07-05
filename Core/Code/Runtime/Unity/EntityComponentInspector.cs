9c536a8906663834f2bb67bc521270567025cac1
Danon5 <danonfive@gmail.com>
2023-07-04 20:22:52 -0700

    - Added inspector component disabling / enabling

--- Core/Code/Runtime/Unity/EntityComponentInspector.cs
+++ Core/Code/Runtime/Unity/EntityComponentInspector.cs
@@ -9,13 +9,13 @@ namespace UFlow.Addon.Ecs.Core.Runtime {
     internal sealed class EntityComponentInspector {
         [SerializeField, FoldoutGroup("Components"), HideLabel, LabelText("Authoring")] 
         [ListDrawerSettings(ShowFoldout = false)]
-        [HideIf(nameof(IsPlaying))]
+        [HideIf(nameof(ShouldDisplayRuntime))]
         private List<EntityComponent> m_authoring = new();
 
         [ShowInInspector, FoldoutGroup("Components"), HideLabel, LabelText("Runtime")] 
         [ListDrawerSettings(ShowFoldout = false, CustomAddFunction = nameof(Add))]
         [OnCollectionChanged(nameof(ApplyRuntimeComponents))]
-        [ShowIf(nameof(IsPlaying))]
+        [ShowIf(nameof(ShouldDisplayRuntime))]
         private List<EntityComponent> m_runtime = new();
 
         private Dictionary<Type, EntityComponent> m_typeMap;
@@ -24,11 +24,11 @@ namespace UFlow.Addon.Ecs.Core.Runtime {
         private Entity m_entity;
         private World m_world;
 
-        private bool IsPlaying => Application.isPlaying;
+        private bool ShouldDisplayRuntime => Application.isPlaying && m_entity.IsAlive();
 
         public void BakeAuthoringComponents(in Entity entity) {
             foreach (var component in m_authoring)
-                entity.SetRaw(component.value);
+                entity.SetRaw(component.value, component.enabled);
             m_typeMap = new Dictionary<Type, EntityComponent>();
             m_typesToSet = new Queue<Type>();
             m_typesToRemove = new Queue<Type>();
@@ -37,6 +37,9 @@ namespace UFlow.Addon.Ecs.Core.Runtime {
         }
 
         public void RetrieveRuntimeComponents() {
+            if (!m_entity.IsAlive()) return;
+            if (m_world == null) return;
+            
             var componentTypes = m_world.GetEntityComponentTypes(m_entity);
 
             // enqueue removes
@@ -61,16 +64,20 @@ namespace UFlow.Addon.Ecs.Core.Runtime {
             while (m_typesToSet.TryDequeue(out var type)) {
                 var componentValue = m_entity.GetRaw(type);
                 if (!m_typeMap.ContainsKey(type)) {
-                    var component = new EntityComponent(componentValue);
+                    var component = new EntityComponent(this, componentValue);
                     m_typeMap.Add(type, component);
                     m_runtime.Add(component);
                 }
                 else
                     m_typeMap[type].value = componentValue;
+                m_typeMap[type].enabled = m_entity.IsEnabledRaw(type);
             }
         }
 
         public void ApplyRuntimeComponents() {
+            if (!m_entity.IsAlive()) return;
+            if (m_world == null) return;
+            
             // enqueue removes
             foreach (var (type, component) in m_typeMap) {
                 if (component.value == null || !m_runtime.Contains(component) || component.value.GetType() != type)
@@ -94,27 +101,49 @@ namespace UFlow.Addon.Ecs.Core.Runtime {
 
             // apply sets
             while (m_typesToSet.TryDequeue(out var type)) {
-                m_entity.SetRaw(m_typeMap[type].value, type);
+                var component = m_typeMap[type];
+                m_entity.SetRaw(type, component.value, component.enabled);
             }
         }
 
         private void Add() {
-            m_runtime.Add(new EntityComponent(default));
+            m_runtime.Add(new EntityComponent(this, default));
+        }
+
+        private void SetEnabled(in Type type, bool enabled) {
+            m_entity.SetEnabledRaw(type, enabled);
         }
 
         [Serializable]
         [HideReferenceObjectPicker]
         private sealed class EntityComponent {
-            [SerializeReference, FoldoutGroup("Default", GroupName = "@this.Name"), InlineProperty, HideLabel]
-            public object value;
+            [ToggleGroup("enabled", "$Name", CollapseOthersOnExpand = false)]
+            [OnValueChanged(nameof(EnabledStateChanged))]
+            public bool enabled;
+
+            [ToggleGroup("enabled", "$Name", CollapseOthersOnExpand = false)]
+            [SerializeReference, InlineProperty, HideLabel]
+            public IEcsComponent value;
+
+            [NonSerialized] 
+            public EntityComponentInspector inspector;
 
             [UsedImplicitly]
             private string Name => value != null ? value.GetType().Name : "None";
 
-            public EntityComponent() { }
+            public EntityComponent() {
+                enabled = true;
+            }
 
-            public EntityComponent(in object value) {
+            public EntityComponent(in EntityComponentInspector inspector, in IEcsComponent value) {
+                this.inspector = inspector;
                 this.value = value;
+                enabled = true;
+            }
+
+            private void EnabledStateChanged() {
+                if (value == null) return;
+                inspector.SetEnabled(value.GetType(), !enabled); // no clue why this has to be inverted
             }
         }
     }
