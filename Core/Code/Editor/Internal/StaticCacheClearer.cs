7b2e26579d44ca37ec0dda6488922f9669771da4
Danon5 <danonfive@gmail.com>
2023-07-09 15:03:19 -0700

    - Progress on serialization addon, added ByteBuffer

--- Core/Code/Editor/Internal/StaticCacheClearer.cs
+++ Core/Code/Editor/Internal/StaticCacheClearer.cs
@@ -1,11 +1,12 @@
-﻿using UFlow.Addon.Ecs.Core.Runtime;
-using UnityEngine;
+﻿using JetBrains.Annotations;
+using UFlow.Addon.Ecs.Core.Runtime;
+using UFlow.Core.Editor;
 
 namespace UFlow.Addon.Ecs.Core.Editor {
+    [UsedImplicitly]
     internal static class StaticCacheClearer {
-        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
-        private static void InitializeOnLoad() {
-            ExternalEngineEvents.clearStaticCachesEvent?.Invoke();
+        static StaticCacheClearer() {
+            UFlowStaticCacheClearer.ClearEvent += () => ExternalEngineEvents.clearStaticCachesEvent?.Invoke();
         }
     }
 }
