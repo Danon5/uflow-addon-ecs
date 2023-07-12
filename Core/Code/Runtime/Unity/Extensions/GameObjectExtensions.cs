ca7eca9495bfd622a60ed24ed824068076f083f9
Danon5 <danonfive@gmail.com>
2023-07-11 20:16:58 -0700

    - Fixed unityengine namespace issues

---
+++ Core/Code/Runtime/Unity/Extensions/GameObjectExtensions.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace UFlow.Addon.Ecs.Core.Runtime {
+    public static class GameObjectExtensions {
+        public static Entity AsEntity(this GameObject gameObject) {
+            return gameObject.GetComponent<SceneEntity>().Entity;
+        }
+    }
+}
