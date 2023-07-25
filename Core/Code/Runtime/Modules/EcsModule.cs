71072323b5dd46fb1bc89ab394572d236adec63f
Danon5 <danonfive@gmail.com>
2023-07-24 17:23:31 -0700

    - Added static utility methods to Modules

--- Core/Code/Runtime/Modules/EcsModule.cs
+++ Core/Code/Runtime/Modules/EcsModule.cs
@@ -1,16 +1,16 @@
 ﻿using UFlow.Core.Runtime;
 
 namespace UFlow.Addon.ECS.Core.Runtime {
-    public sealed class EcsModule : BaseBehaviourModule {
+    public sealed class EcsModule : BaseBehaviourModule<EcsModule> {
         public World World { get; private set; }
         
-        public override void Load() {
+        public override void LoadDirect() {
             World = EcsUtils.Worlds.CreateWorldFromType<DefaultWorld>();
             World.SetupSystemGroups();
             World.WhenReset(() => World.ResetSystemGroups());
         }
 
-        public override void Unload() {
+        public override void UnloadDirect() {
             World.Destroy();
         }
 
