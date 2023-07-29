40db9854b0d60bd0a62df2d5e9cee134456949f3
Danon5 <danonfive@gmail.com>
2023-07-29 15:03:07 -0700

    - Removed unused delegates

--- Core/Code/Runtime/Serialization/Internal/Delegates.cs
+++
@@ -1,4 +0,0 @@
-﻿namespace UFlow.Addon.ECS.Core.Runtime {
-    public delegate void SerializeDelegate<T>(in ByteBuffer buffer, ref T obj);
-    public delegate void DeserializeDelegate<T>(in ByteBuffer buffer, ref T obj);
-}
