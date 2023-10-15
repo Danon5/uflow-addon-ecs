07f74abbcb5e75ac01e73724941863e9422a9620
Danon5 <danonfive@gmail.com>
2023-10-15 03:08:23 -0700

    improved SceneEntityRef

--- Core/Code/Runtime/SceneEntityRef.cs
+++ Core/Code/Runtime/SceneEntityRef.cs
@@ -5,12 +5,16 @@ namespace UFlow.Addon.ECS.Core.Runtime {
     [Serializable]
     [HideLabel]
     public struct SceneEntityRef {
+#if UNITY_EDITOR
         [LabelText("@$property.Parent.NiceName")] 
-        [SuffixLabel("@this.IsPlaying ? this.EntityName : string.Empty")]
+        [SuffixLabel("@" + nameof(IsPlaying) + " ? " + nameof(EntityName) + " : string.Empty")]
+#endif
         public SceneEntity sceneEntity;
         
         public Entity Entity => sceneEntity == null ? default : sceneEntity.Entity;
+#if UNITY_EDITOR
         private bool IsPlaying => sceneEntity != null && sceneEntity.IsPlaying;
         private string EntityName => Entity.ToString();
+#endif
     }
 }
